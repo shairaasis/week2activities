8560d78a0fa8505741fd68191dc3f9b52121df70
Shaira Asis <shairaasis99@gmail.com>
2022-04-17 03:58:33 +0800

    Added Main and Car java files for Week2 Activities

--- Car.java
+++ Car.java
@@ -1,81 +1,102 @@
-public class Car extends Main{
-    String name;
-    String model;
-    String brand;
-    int year;
-    String location;
+import java.util.Scanner;
+public class Car{
+     String name;
+     String model;
+     String brand;
+     int year;
+     private static String location;
     
-
+    //Car constructor
     public Car(String name, String model, String brand, int year, String location){
         this.name = name;
         this.model = model;
         this.brand = brand;
         this.year = year;
-        this.location = location;
+        Car.location = location;
     }
 
-    public static void travel(Car car1) {
-        car1.setLocation("Leyte");
-        System.out.println("Name: " + car1.getName() + "\nModel: " + car1.getModel()+ "\nBrand: " +  car1.getBrand()+ "\nYear: " + car1.getYear()+ "\nLcation: " + car1.getLocation());
-        
+    //Method for displaying current location (Activity 1)
+    public static void displayCurrentLocation(Car cars){
+        System.out.println("\n*Initial Location*");
+        System.out.println("Name: " + cars.getName() + "\nModel: " + cars.getModel()+ "\nBrand: " +  cars.getBrand()+ "\nYear: " + cars.getYear()+ "\nLocation: " + Car.getLocation());
     }
-    
+
+
+    //Method for setting the place travelled(destination) and displaying location after travelling (Activity 1 and 2)
+    public static void travel(String location) {
+        //setDestination("Leyte");
+        setLocation("Leyte");
+        System.out.println("\n*After Travelling*");
+        System.out.println("Location: " + getLocation());
+    }
+
+    //Method for setting the place travelled(destination) and calls the method for displaying location after travelling and gas remaining (Activity 3)
+    public static void travel(Car cars) {
+        Scanner sc = new Scanner(System.in);  
+        double gas = 10;    
+        System.out.print("Enter destination: ");
+        String destination = sc.nextLine();
+        System.out.print("Enter the distance: ");
+        double distance = sc.nextDouble();
+        //Call calculateGasUsed method and Set the gas of the car to gas deducted by the gas used
+        gas=gas-calculateGasUsed(distance);
+        displayCurrentLocationAndGasRemaining(gas,destination);
+        sc.close();
+    }
+    //Method for calculating gas used (Activity 3)
+    public static double calculateGasUsed(double distance) {
+        double kmPerLitter = 15.5;
+        return kmPerLitter / distance;
+    }
+    //Method for displaying location after travelling and gas remaining (Activity 3)
+    public static void displayCurrentLocationAndGasRemaining(double gas, String destination){
+        System.out.print("\nAfter travelling from" +Car.getLocation()+ " to ");
+        //set location to destination
+        Car.setLocation(destination);
+        System.out.print(destination +", the remaining Gas is " + gas + " litter(s).");
+    }
 
     public String getName() {
         return name;
     }
 
-
     public void setName(String name) {
         this.name = name;
     }
-
 
     public String getModel() {
         return model;
     }
 
-
     public void setModel(String model) {
         this.model = model;
     }
-
 
     public String getBrand() {
         return brand;
     }
 
-
     public void setBrand(String brand) {
         this.brand = brand;
     }
-
 
     public int getYear() {
         return year;
     }
 
-
     public void setYear(int year) {
         this.year = year;
     }
 
-
-    public String getLocation() {
+    
+    public static String getLocation() {
         return location;
     }
 
-
-    public void setLocation(String location) {
-        this.location = location;
+    public static void setLocation(String location) {
+        Car.location = location;
     }
 
-
-
-
-
-
-
-
-
+    
+    
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,49 @@
+import java.util.Scanner;
 public class Main {
 
 
     public static void main(String[] args) {
-        Car car1 = new Car("Tesla Model Y", "Model Y", "Tesla", 2022, "Cebu");
-        System.out.println("\n*Initial Location*");
-        System.out.println("Name: " + car1.getName() + "\nModel: " + car1.getModel()+ "\nBrand: " +  car1.getBrand()+ "\nYear: " + car1.getYear()+ "\nLcation: " + car1.getLocation());
-        System.out.println("\n*After Travelling*");
-        //Call travel method and pass the object
-        Car.travel(car1);
+        Scanner sc = new Scanner(System.in);
+        //Create a Car class array (Activity 2)
+        Car[] cars = new Car[3];
+        //Instantiate a car and give it values (Activity 1)
+        cars[0] = new Car("Tesla Model Y", "Model Y", "Tesla", 2022, "Cebu");
+        //Call method to display car info including current location (Activity 1)
+        System.out.println("\nCar # 1");
+        Car.displayCurrentLocation(cars[0]);
+        //Call travel method and set the destination
+        Car.travel(Car.getLocation());
+
+
+        //Activity 2 starts here. Instantiate and give values
+        for(int index=1 ; index<cars.length; index++){
+            System.out.println("\nCar # " +(index+1));
+            System.out.print("Enter car name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Enter car brand: ");
+            String brand = sc.nextLine();
+
+            System.out.print("Enter car model: ");
+            String model = sc.nextLine();
+
+            System.out.print("Enter car year: ");
+            int year = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Enter car location: ");
+            String location = sc.nextLine();
+            System.out.print("\n");
+            
+            cars[index] = new Car(name, model, brand, year, location);
+            Car.displayCurrentLocation(cars[index]);
+            Car.travel(Car.getLocation());
+            //Activity 3 starts here(Call travel method)
+            Car.travel(cars[index]);
+            //Car.displayCurrentLocation(cars[index]);
+        }
+        sc.close();
+    
     }
     
 }
